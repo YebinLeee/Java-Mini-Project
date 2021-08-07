d74d8fe3713dbf0d5d4679934952e0486c7530e3
이예빈 <lyb2325@gmail.com>
2021-08-07 15:20:05 +0900

    Frame, Panel, Label 추가

--- src/Frame.java
+++ src/Frame.java
@@ -9,9 +9,9 @@ public class Frame {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel(); // 프레임 안에 여러 개의 패널 추가 가능
 		
-		frame.add(panel);
+		panel.add(new JLabel("Welcome to this Project! I'm Yebin :) ")); // 레이블 추가
+		frame.add(panel); // 페널 추가
 		
-		panel.add(new JLabel("Welcome to this Project! I'm Yebin :) "));
 		frame.setResizable(false); // 사용자가 임의로 프레임 사이즈 변경 X
 		frame.setVisible(true); // 실행 후 가시적으로 보일 수 있게 설정
 		frame.setPreferredSize(new Dimension(840, 8490/4*3)); // 직사각형의 가로/세로 설정
