ecbc100e8485ce0693f616eee8f0d07edc45b710
이예빈 <lyb2325@gmail.com>
2021-08-07 15:42:37 +0900

    JTextArea, JTextField 추가 - 입력받는 텍스트 필드 추가

--- src/Frame.java
+++ src/Frame.java
@@ -5,6 +5,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JTextArea;
+// import javax.swing.JTextField;
 
 public class Frame {
 	public static void main(String[] args) {
@@ -12,10 +14,14 @@ public class Frame {
 		JPanel panel = new JPanel(); // 패널 추가. (프레임 안에 여러 개의 패널 추가 가능)
 		JLabel label = new JLabel("MY JAVA MINI PROJECT. Welcome !"); // text 문구 추가
 		JButton btn1 = new JButton("Click Me!"); // 버튼 추가
+		JTextArea txtArea = new JTextArea();  // 여러 줄의 텍스트 입력 (JTextField = 한 줄의 텍스트 입력)
+		// JTextField txtField = new JTextField(200); // 
 		
 		panel.setLayout(new BorderLayout()); // 패널을 원하는 특정한 위치에 이동
 		panel.add(label, BorderLayout.NORTH); // 레이블을 위쪽에 추가
 		panel.add(btn1, BorderLayout.WEST); // 버튼을 왼쪽에 추가
+		panel.add(txtArea, BorderLayout.CENTER); // textArea를 가운데에 추가
+		// panel.add(txtField, BorderLayout.CENTER);
 		
 		frame.add(panel); // 프레임에 페널 추가
 		
