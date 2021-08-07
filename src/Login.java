1efb91603d80ed21b81b6df18669513c3f621384
이예빈 <lyb2325@gmail.com>
2021-08-07 16:16:13 +0900

    로그인 클릭 시 이벤트 - 성공/실패시 메세지창 출력

--- src/Login.java
+++ src/Login.java
@@ -1,8 +1,14 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 
 public class Login extends JFrame{
 	public Login() {
@@ -12,11 +18,35 @@ public class Login extends JFrame{
 		JTextField txtID = new JTextField(10);
 		JPasswordField txtPass = new JPasswordField(10); // 패스워드
 		
+		/* ID, PW 입력 후 클릭 버튼 */
+		JButton logBtn = new JButton("Log In");
+		
 		panel.add(label);
 		panel.add(txtID);
 		panel.add(pswrd);
 		panel.add(txtPass);
+		panel.add(logBtn);
 		
+		logBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// ID, PW 입력받아 텍스트 비교
+				String id = "Yebin";
+				String pw = "1234";
+				
+				// id, pw 모두 일치하는 경우 로그인 성공한 메세지 창 출력
+				if(id.equals(txtID.getText()) && pw.equals(txtPass.getText())) {
+					JOptionPane.showMessageDialog(null, "You have logged in successfully");
+				}
+				// 로그인 실패 메세지
+				else {
+					JOptionPane.showMessageDialog(null, "You failed to log in");
+				}
+				
+			}
+			
+		});
 		add(panel);
 		
 		setVisible(true); 
