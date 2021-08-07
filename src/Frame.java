6c94e078e638e15a9afcdffcb6c4ec8e02f50601
이예빈 <lyb2325@gmail.com>
2021-08-07 15:26:41 +0900

    버튼, 레이블추가, BorderLayout으로 위치 설정

--- src/Frame.java
+++ src/Frame.java
@@ -1,16 +1,23 @@
 import java.awt.Dimension;
+import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class Frame {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		JPanel panel = new JPanel(); // 프레임 안에 여러 개의 패널 추가 가능
+		JPanel panel = new JPanel(); // 패널 추가. (프레임 안에 여러 개의 패널 추가 가능)
+		JLabel label = new JLabel("MY JAVA MINI PROJECT. Welcome !"); // text 문구 추가
+		JButton btn1 = new JButton("Click Me!"); // 버튼 추가
 		
-		panel.add(new JLabel("Welcome to this Project! I'm Yebin :) ")); // 레이블 추가
-		frame.add(panel); // 페널 추가
+		panel.setLayout(new BorderLayout()); // 패널을 원하는 특정한 위치에 이동
+		panel.add(label, BorderLayout.NORTH); // 레이블을 위쪽에 추가
+		panel.add(btn1, BorderLayout.WEST); // 버튼을 왼쪽에 추가
+		
+		frame.add(panel); // 프레임에 페널 추가
 		
 		frame.setResizable(false); // 사용자가 임의로 프레임 사이즈 변경 X
 		frame.setVisible(true); // 실행 후 가시적으로 보일 수 있게 설정
