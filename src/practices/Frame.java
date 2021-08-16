079f48a8a5562efcae3da537a8ad6bed2f9e7427
이예빈 <lyb2325@gmail.com>
2021-08-16 17:28:33 +0900

    Timer Runnable Thread practices

---
+++ src/practices/Frame.java
@@ -0,0 +1,62 @@
+package practices;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+/*
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+*/
+
+public class Frame {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel(); // 패널 추가. (프레임 안에 여러 개의 패널 추가 가능)
+		JLabel label = new JLabel("MY JAVA MINI PROJECT. Welcome !"); // text 문구 추가
+		JButton btn1 = new JButton("Click Me!"); // 버튼 추가
+		JTextArea txtArea = new JTextArea();  // 여러 줄의 텍스트 입력 (JTextField = 한 줄의 텍스트 입력)
+		// JTextField txtField = new JTextField(200); // 
+		
+		JPanel btnPanel = new JPanel(); // 버튼들을 넣을 패널 추가
+		JButton btn2 = new JButton("Exit"); // 프로그램을 종료하는 버튼 추가
+		
+		panel.setLayout(new BorderLayout()); // 패널을 원하는 특정한 위치에 이동
+		btnPanel.add(btn1);
+		btnPanel.add(btn2);
+		
+		panel.add(label, BorderLayout.NORTH); // 레이블을 위쪽에 추가
+		panel.add(btnPanel, BorderLayout.WEST); // 버튼을 왼쪽에 추가
+		panel.add(txtArea, BorderLayout.CENTER); // textArea를 가운데에 추가
+		// panel.add(txtField, BorderLayout.CENTER);
+		
+		btn1.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//txtArea.append("You are amazing!\n");
+				label.setText(txtArea.getText()); // txtArea에서 입력받은 텍스트로 레이블을 변경
+			}
+		});
+		btn2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0); // 프로그램 종료
+			}
+		});
+		
+		frame.add(panel); // 프레임에 페널 추가
+		
+		frame.setResizable(false); // 사용자가 임의로 프레임 사이즈 변경 X
+		frame.setVisible(true); // 실행 후 가시적으로 보일 수 있게 설정
+		frame.setPreferredSize(new Dimension(840, 8490/4*3)); // 직사각형의 가로/세로 설정
+		frame.setSize(840, 840/4*3);
+		frame.setLocationRelativeTo(null); // 가운데에 프레임이 출력되도록 설정
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프로그램을 종료했을 때 모든 실행도 종료되도록
+	}
+}
+
---
+++ src/practices/MyTimerThread.java
@@ -0,0 +1,114 @@
+package practices;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+import java.awt.*;
+
+
+// Runnable 인터페이스를 구현하는 타이머 스레드 TimerRunnable
+class TimerRunnable implements Runnable{
+	private JLabel timerLabel; // 타이머 값이 출력되는 레이블
+	
+	public TimerRunnable(JLabel timerLabel) {
+		this.timerLabel = timerLabel; // 타이머 카운트 출력하는 레이블
+	}
+	
+	// 스레드 코드. run() 종료하면 스레드 종료
+	public void run() {
+		int n=0;
+		while(true) {
+			timerLabel.setText(Integer.toString(n));
+			n++;
+			try {
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e) {return;}
+		}
+	}
+}
+
+// start, stop 버튼을 가진 ButtonPannel
+class ButtonPanel extends JPanel{
+	private JButton startButton;
+	private JButton stopButton;
+	
+	Border emptyBorder= BorderFactory.createEmptyBorder();
+	
+	// Constructor
+	ButtonPanel()
+	{
+		this.setLayout(new FlowLayout());
+		//this.setPreferredSize(new Dimension(300,60));
+		this.setBackground(Color.gray);
+		
+		startButton = new JButton("START");
+		startButton.setBorder(emptyBorder);
+		startButton.setFont(new Font("IM혜민 Regular", Font.PLAIN, 20));
+		startButton.setBackground(Color.RED);
+		
+		this.add(startButton);
+		this.add(Box.createHorizontalStrut(30));
+		
+		stopButton = new JButton("STOP");
+		stopButton.setBorder(emptyBorder);
+		stopButton.setFont(new Font("IM혜민 Regular", Font.PLAIN, 20));
+		stopButton.setBackground(Color.GREEN);
+		
+		this.add(stopButton);
+	}
+	
+	public JButton getStart() {
+		return startButton;
+	}
+	
+	public JButton getStop() {
+		return stopButton;
+	}
+	
+
+}
+
+public class MyTimerThread extends JFrame{
+	
+	private ButtonPanel btnPanel;
+	
+	private JButton startBtn;
+	private JButton stopBtn;
+	
+	public MyTimerThread() {
+		setTitle("My Timer");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout());
+		
+		// 타이머 값 출력하는 패널 생성
+		JPanel timerPanel = new JPanel();
+		JLabel timerLabel = new JLabel();
+		timerLabel.setFont(new Font("IM혜민체", Font.ITALIC, 80));
+		timerPanel.add(timerLabel, BorderLayout.CENTER);
+		c.add(timerPanel, BorderLayout.CENTER);
+		
+		// 타이머 스레드로 사용할 Runnable 객체 생성
+		TimerRunnable runnable = new TimerRunnable(timerLabel);
+		Thread th = new Thread(runnable);
+		
+		setSize(250, 200);
+		setVisible(true);
+		
+		// start, stop 버튼이 있는 btnPannel
+		btnPanel = new ButtonPanel();
+		startBtn = btnPanel.getStart();
+		stopBtn = btnPanel.getStop();
+		
+		c.add(btnPanel, BorderLayout.SOUTH);
+		
+		th.start(); // 스레드 시작
+	}
+	
+	public static void main(String[] args) {
+		new MyTimerThread();
+	}
+
+}
